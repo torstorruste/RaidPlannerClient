4482bfdd28818514451992226f12cba53570a0eb
Tor Storruste <tor@superhelt.org>
2020-09-20 12:04:06 +0200

    Can add new players

--- Backing/Players.razor.cs
+++ Backing/Players.razor.cs
@@ -4,9 +4,7 @@ namespace org.superhelt.Blazor
 {
     public partial class Players {
 
-
-        public List<Player> GetPlayers() {
-            return new List<Player>{
+        List<Player> players  = new List<Player>{
                 new Player{Id=1, Name="Zikura", Characters=new List<Character>{
                     new Character{Id=1, Name="Zikura", CharacterClass=CharacterClass.Druid, Roles=new List<Role>{Role.Tank, Role.Ranged}},
                     new Character{Id=2, Name="Ziktator", CharacterClass=CharacterClass.Monk, Roles=new List<Role>{Role.Tank, Role.Ranged}}
@@ -15,6 +13,13 @@ namespace org.superhelt.Blazor
                     new Character{Id=3, Name="Drahc", CharacterClass=CharacterClass.Druid, Roles=new List<Role>{Role.Tank, Role.Melee}}
                 }}
             };
+
+        public List<Player> GetPlayers() {
+            return players;
+        }
+
+        public void AddPlayer() {
+            players.Add(new Player{Id=3, Name="Furo"});
         }
     }
 }
