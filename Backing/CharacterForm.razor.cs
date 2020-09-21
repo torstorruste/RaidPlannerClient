54cec585b1af3e72d87dfcd929fc852760afd68c
Tor Storruste <tor@superhelt.org>
2020-09-21 23:17:55 +0200

    Added images for classes and roles

--- Backing/CharacterForm.razor.cs
+++ Backing/CharacterForm.razor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Components;
 using RaidPlannerClient.Model;
 using RaidPlannerClient.Service;
@@ -19,9 +20,19 @@ namespace RaidPlannerClient.Components
         [Inject]
         public ICharacterService characterService { get; set; }
 
+        public Boolean IsTank { get; set; }
+        public Boolean IsHealer { get; set; }
+        public Boolean IsMelee { get; set; }
+        public Boolean IsRanged { get; set; }
+
         public async void HandleValidSubmit()
         {
             Console.WriteLine("CharacterForm::HandleValidSubmit");
+            Console.WriteLine($"Is Tank: {IsTank}");
+            Console.WriteLine($"Is Healer: {IsHealer}");
+            Console.WriteLine($"Is Melee: {IsMelee}");
+            Console.WriteLine($"Is Ranged: {IsRanged}");
+            Character.Roles = GetRolesFromCheckboxes();
             if (Character.Id == null)
             {
                 Character = await characterService.AddCharacter(Player, Character);
@@ -31,6 +42,33 @@ namespace RaidPlannerClient.Components
             {
                 characterService.UpdateCharacter(Player, Character);
                 PlayerForm.UpdateCharacters();
+            }
+        }
+
+        public List<Role> GetRolesFromCheckboxes() {
+            List<Role> roles = new List<Role>();
+
+            if(IsTank) roles.Add(Role.Tank);
+            if(IsHealer) roles.Add(Role.Healer);
+            if(IsMelee) roles.Add(Role.Melee);
+            if(IsRanged) roles.Add(Role.Ranged);
+
+            return roles;
+        }
+
+        public void SetRolesFromCharacter(List<Role> roles) {
+            IsTank = roles.Contains(Role.Tank);
+            IsHealer = roles.Contains(Role.Healer);
+            IsMelee = roles.Contains(Role.Melee);
+            IsRanged = roles.Contains(Role.Ranged);
+        }
+
+        protected override void OnParametersSet() {
+            if(Character != null && Character.Roles != null) {
+                Console.WriteLine("Character is set, updating checkboxes");
+                SetRolesFromCharacter(Character.Roles);
+            } else {
+                Console.WriteLine("Character is not set");
             }
         }
     }
